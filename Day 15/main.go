206ca4ef009eb05e30dbd512781405888fc6098c
RainbowDog98 <42966213+RainbowDog98@users.noreply.github.com>
2020-12-15 22:40:56 +0200

    Day 15 code optimization
    Instead of making new structs and pushing then into the hashmap it now pushes struct pointers instead. Performance time is now about ~2x less.

--- Day 15/main.go
+++ Day 15/main.go
@@ -22,36 +22,36 @@ func main() {
 	for turn, i := range startingNum {
 		num, _ := strconv.Atoi(i)
 		previousNumber = num
-		hash.Put(num, Storage{turn: turn+1, firstTimeSpoken: true})
+		hash.Put(num, &Storage{turn: turn+1, firstTimeSpoken: true})
 	}
 
 	turn := len(startingNum)
 	t := time.Now()
 	for turn != 30000000 {
-		nextNumber := -1
+		var nextNumber int
 		tmp, _ := hash.Get(previousNumber)
-		val := tmp.(Storage)
+		val := tmp.(*Storage)
 
 		if val.firstTimeSpoken{
 			nextNumber = 0
 			told , found := hash.Get(0)
 			if !found { // 0 is a new value
-				hash.Put(0, Storage{turn: turn+1, firstTimeSpoken: true})
+				hash.Put(0, &Storage{turn: turn+1, firstTimeSpoken: true})
 			} else { // 0 already exists in the set
-				old := told.(Storage)
-				hash.Put(0, Storage{turn: old.turn, firstTimeSpoken: false})
+				old := told.(*Storage)
+				old.firstTimeSpoken = false
 			}
 		} else {
 			nextNumber = turn - val.turn
 			told , found := hash.Get(nextNumber)
 			if !found { // nextNumber is a new value
-				hash.Put(nextNumber, Storage{turn: turn+1, firstTimeSpoken: true})
+				hash.Put(nextNumber, &Storage{turn: turn+1, firstTimeSpoken: true})
 			} else { // nextNumber already exist in the set
-				old := told.(Storage)
-				hash.Put(nextNumber, Storage{turn: old.turn, firstTimeSpoken: false})
+				old := told.(*Storage)
+				old.firstTimeSpoken = false
 			}
 			// Update the current number to his current turn as last turn
-			hash.Put(previousNumber, Storage{turn: turn, firstTimeSpoken: false})
+			val.turn = turn;
 		}
 		turn++
 		previousNumber = nextNumber
